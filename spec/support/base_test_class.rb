0bd7826afd492d0a5186cfa9fe9c55bb739d9a0b
Miguel Diaz <miguel.diaz@codescrum.com>
2014-11-06 14:42:10 -0500

    simplify base test class using Struct

--- spec/support/base_test_class.rb
+++ spec/support/base_test_class.rb
@@ -1,17 +1,12 @@
 # this is just a class that includes convenience methods for testing
 # such as a giving it a name so that is easy to inspect
-class BaseTestClass
-  attr_accessor :name
-
-  def initialize(name)
-    self.name = name
-  end
+BaseTestClass = Struct.new(:name) do
 
   def to_s
     name
   end
 
   def inspect
-    "\"#{to_s}\""
+    "\"#{self}\""
   end
 end

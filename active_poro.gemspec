f943f89ae7b6f3b668923b0d043e702ce4505c9b
Miguel Diaz <miguel.diaz@codescrum.com>
2014-11-18 15:44:14 -0500

    add test coverage setup

--- active_poro.gemspec
+++ active_poro.gemspec
@@ -22,6 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency('rspec')
   spec.add_development_dependency('jazz_hands')
+  spec.add_development_dependency('simplecov', '0.9.0')
+  spec.add_development_dependency('codeclimate-test-reporter', '0.4.0')
 
   spec.add_runtime_dependency('activesupport')
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
+# Add coverage with simple_cov and codeclimate
+# These must be the first lines in the file
+require 'codeclimate-test-reporter'
+require 'simplecov'
+
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+formatters << CodeClimate::TestReporter::Formatter if ENV['CODECLIMATE_REPO_TOKEN']
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+#######################################################################
+
 require 'rubygems'
 require 'pry'
 
